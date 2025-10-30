1a50e4f30073ccfad1ba8b497f1e13c7ccde0921
Haris Raees <haris.pixako1@gmail.com>
2025-10-30 11:13:10 +0500

    created blog page

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+  return (
+    <main className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold text-green-600">About Page</h1>
+      <p className="mt-4 text-gray-700">This is the About page using Pages Router.</p>
+    </main>
+  );
+}
+
+export default AboutPage;
+
